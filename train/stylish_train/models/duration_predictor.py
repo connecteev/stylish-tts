aeef83878fa3732aaa4aa73731ff8f8b642b5ab8
H.D.Tài <tai17082006.tk@gmail.com>
2025-06-05 03:38:21 +0700

    Experiment on Transformer-based DurationEncoder

--- train/stylish_train/models/duration_predictor.py
+++ train/stylish_train/models/duration_predictor.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange
 from .common import LinearNorm
+from .text_encoder import FFN, MultiHeadAttention
 
 
 # class DurationPredictor(nn.Module):
@@ -56,84 +57,144 @@ class DurationPredictor(nn.Module):
         return duration.squeeze(-1)
 
 
+# class DurationEncoder(nn.Module):
+#     def __init__(self, sty_dim, d_model, nlayers, dropout=0.1):
+#         super().__init__()
+#         self.lstms = nn.ModuleList()
+#         for _ in range(nlayers):
+#             self.lstms.append(
+#                 nn.LSTM(
+#                     d_model + sty_dim,
+#                     d_model // 2,
+#                     num_layers=1,
+#                     batch_first=True,
+#                     bidirectional=True,
+#                     dropout=0,
+#                 )
+#             )
+#             self.lstms.append(AdaLayerNorm(sty_dim, d_model))
+
+#         self.dropout = dropout
+#         self.d_model = d_model
+#         self.sty_dim = sty_dim
+
+#     def forward(self, x, style, text_lengths, m):
+#         masks = m.to(text_lengths.device)
+
+#         x = torch.cat([x, style], dim=1)
+#         x = x.permute(2, 0, 1)
+#         x.masked_fill_(masks.unsqueeze(-1).transpose(0, 1), 0.0)
+
+#         x = x.transpose(0, 1)
+#         input_lengths = text_lengths.cpu()
+#         x = x.transpose(-1, -2)
+
+#         for block in self.lstms:
+#             if isinstance(block, AdaLayerNorm):
+#                 x = block(x.transpose(-1, -2), style).transpose(-1, -2)
+#                 x = torch.cat([x, style], dim=1)
+#                 x.masked_fill_(masks.unsqueeze(-1).transpose(-1, -2), 0.0)
+#             else:
+#                 x = x.transpose(-1, -2)
+#                 x = nn.utils.rnn.pack_padded_sequence(
+#                     x, input_lengths, batch_first=True, enforce_sorted=False
+#                 )
+#                 block.flatten_parameters()
+#                 x, _ = block(x)
+#                 x, _ = nn.utils.rnn.pad_packed_sequence(x, batch_first=True)
+#                 x = F.dropout(x, p=self.dropout, training=self.training)
+#                 x = x.transpose(-1, -2)
+
+#                 x_pad = torch.zeros([x.shape[0], x.shape[1], m.shape[-1]])
+
+#                 x_pad[:, :, : x.shape[-1]] = x
+#                 x = x_pad.to(x.device)
+
+#         return x.transpose(-1, -2)
+
+#     def infer(self, x, style):
+#         """
+#         x: (batch, channels, tokens)
+#         style: (batch, embedding)
+#         """
+#         # s = rearrange(style, "b e -> b e 1")
+#         # s = s.expand(-1, -1, x.shape[2])  # batch embedding tokens
+#         x = torch.cat([x, style], dim=1)
+
+#         for block in self.lstms:
+#             if isinstance(block, AdaLayerNorm):
+#                 x = rearrange(x, "b c t -> b t c")
+#                 x = block(x, style)
+#                 x = rearrange(x, "b t c -> b c t")
+#                 x = torch.cat([x, style], dim=1)
+#             else:
+#                 x = rearrange(x, "1 c t -> t c")
+#                 block.flatten_parameters()
+#                 x, _ = block(x)
+#                 x = F.dropout(x, p=self.dropout, training=self.training)
+#                 x = rearrange(x, "t c -> 1 c t")
+
+#         return rearrange(x, "b c t -> b t c")
+
+
 class DurationEncoder(nn.Module):
-    def __init__(self, sty_dim, d_model, nlayers, dropout=0.1):
+    def __init__(
+        self,
+        sty_dim,
+        d_model,
+        nlayers,
+        dropout=0.1,
+        n_heads=2,
+        kernel_size=1,
+        **kwargs,
+    ):
         super().__init__()
-        self.lstms = nn.ModuleList()
-        for _ in range(nlayers):
-            self.lstms.append(
-                nn.LSTM(
-                    d_model + sty_dim,
-                    d_model // 2,
-                    num_layers=1,
-                    batch_first=True,
-                    bidirectional=True,
-                    dropout=0,
+        hidden_channels = d_model + sty_dim
+        self.n_heads = n_heads
+        self.n_layers = nlayers
+        self.kernel_size = kernel_size
+
+        self.drop = torch.nn.Dropout(dropout)
+        self.attn_layers = torch.nn.ModuleList()
+        self.norm_layers_1 = torch.nn.ModuleList()
+        self.ffn_layers = torch.nn.ModuleList()
+        self.norm_layers_2 = torch.nn.ModuleList()
+        self.proj_layers = torch.nn.ModuleList()
+
+        for _ in range(self.n_layers):
+            self.attn_layers.append(
+                MultiHeadAttention(
+                    hidden_channels, hidden_channels, n_heads, p_dropout=dropout
                 )
             )
-            self.lstms.append(AdaLayerNorm(sty_dim, d_model))
-
-        self.dropout = dropout
-        self.d_model = d_model
-        self.sty_dim = sty_dim
-
-    def forward(self, x, style, text_lengths, m):
-        masks = m.to(text_lengths.device)
-
-        x = torch.cat([x, style], dim=1)
-        x = x.permute(2, 0, 1)
-        x.masked_fill_(masks.unsqueeze(-1).transpose(0, 1), 0.0)
-
-        x = x.transpose(0, 1)
-        input_lengths = text_lengths.cpu()
-        x = x.transpose(-1, -2)
-
-        for block in self.lstms:
-            if isinstance(block, AdaLayerNorm):
-                x = block(x.transpose(-1, -2), style).transpose(-1, -2)
-                x = torch.cat([x, style], dim=1)
-                x.masked_fill_(masks.unsqueeze(-1).transpose(-1, -2), 0.0)
-            else:
-                x = x.transpose(-1, -2)
-                x = nn.utils.rnn.pack_padded_sequence(
-                    x, input_lengths, batch_first=True, enforce_sorted=False
+            self.norm_layers_1.append(AdaLayerNorm(self.sty_dim, hidden_channels))
+            self.ffn_layers.append(
+                FFN(
+                    hidden_channels,
+                    hidden_channels,
+                    hidden_channels * 2,
+                    kernel_size,
+                    p_dropout=dropout,
                 )
-                block.flatten_parameters()
-                x, _ = block(x)
-                x, _ = nn.utils.rnn.pad_packed_sequence(x, batch_first=True)
-                x = F.dropout(x, p=self.dropout, training=self.training)
-                x = x.transpose(-1, -2)
+            )
+            self.norm_layers_2.append(AdaLayerNorm(self.sty_dim, hidden_channels))
+            self.proj_layers.append(nn.Conv1d(hidden_channels, d_model, 1))
 
-                x_pad = torch.zeros([x.shape[0], x.shape[1], m.shape[-1]])
+    def forward(self, x, style, x_mask):
+        attn_mask = x_mask.unsqueeze(2) * x_mask.unsqueeze(-1)
+        for i in range(self.n_layers):
+            x = torch.cat([x, style], dim=1)
+            x = x * x_mask
+            y = self.attn_layers[i](x, x, attn_mask)
+            y = self.drop(y)
+            x = self.norm_layers_1[i](x + y, style)
 
-                x_pad[:, :, : x.shape[-1]] = x
-                x = x_pad.to(x.device)
-
-        return x.transpose(-1, -2)
-
-    def infer(self, x, style):
-        """
-        x: (batch, channels, tokens)
-        style: (batch, embedding)
-        """
-        # s = rearrange(style, "b e -> b e 1")
-        # s = s.expand(-1, -1, x.shape[2])  # batch embedding tokens
-        x = torch.cat([x, style], dim=1)
-
-        for block in self.lstms:
-            if isinstance(block, AdaLayerNorm):
-                x = rearrange(x, "b c t -> b t c")
-                x = block(x, style)
-                x = rearrange(x, "b t c -> b c t")
-                x = torch.cat([x, style], dim=1)
-            else:
-                x = rearrange(x, "1 c t -> t c")
-                block.flatten_parameters()
-                x, _ = block(x)
-                x = F.dropout(x, p=self.dropout, training=self.training)
-                x = rearrange(x, "t c -> 1 c t")
-
-        return rearrange(x, "b c t -> b t c")
+            y = self.ffn_layers[i](x, x_mask)
+            y = self.drop(y)
+            x = self.norm_layers_2[i](x + y, style)
+            x = self.proj_layers[i](x)
+        x = x * x_mask
+        return x
 
 
 class AdaLayerNorm(nn.Module):
