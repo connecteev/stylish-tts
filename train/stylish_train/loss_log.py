02cfffd8c9889e54dd5c7dcbe500256c96894a63
Jonathon Duerig <>
2025-07-11 19:54:22 +0000

    Fix model name for lr reporting, update config file

--- train/stylish_train/loss_log.py
+++ train/stylish_train/loss_log.py
@@ -42,7 +42,7 @@ class LossLog:
             )
         else:
             writer_type = "train"
-            lr = stage.optimizer.optimizers["text_encoder"].param_groups[0]["lr"]
+            lr = stage.optimizer.optimizers["speech_predictor"].param_groups[0]["lr"]
             lr_string = f", lr: {lr:.7f}"
             self.logger.info(
                 f"Epoch [{manifest.current_epoch}/{stage.max_epoch}], "
